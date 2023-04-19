a60f1c29a8e76f4358e005ecd097729a83cfe688
ICI <ICI@DESKTOP-EA5M9P6>
2023-04-19 11:08:45 +0900

    form / controller 연습 예제

---
+++ src/main/java/com/gyojincompany/member/MemberController.java
@@ -0,0 +1,68 @@
+package com.gyojincompany.member;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.gyojincompany.member.dto.MemberDto;
+
+@Controller
+public class MemberController {
+	//login -> loginOk.jsp가 출력되는 RequestMapping
+	//id:tiger, pw:12345 파라미터가 전송됨(loginOk?id=tiger&pw=12345)
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping(value = "/login2")
+	public String login2() {
+		return "login2";
+	}
+	
+	@RequestMapping(value = "/join")
+	public String join() {
+		return "join";
+	}
+	
+	@RequestMapping(value = "/joinOk")
+	public String joinOk(MemberDto memberDto, Model model) {
+		
+		//System.out.println(memberDto.getName());
+		model.addAttribute("memberDto", memberDto);
+		
+		
+		return "joinOk";
+	}
+	
+	@RequestMapping(value = "/loginOk2")
+	public String loginOk2(@RequestParam("id") String mid, @RequestParam("pw") String mpw, Model model) {
+		
+		model.addAttribute("memberId", mid);
+		
+		return "loginOk";
+	}
+	
+	@RequestMapping(value = "/loginOk")
+	public String loginOk(HttpServletRequest request, Model model) {
+		
+		String mid = request.getParameter("id");
+		String mpw = request.getParameter("pw");
+		
+		model.addAttribute("memberId", mid);
+		
+		return "loginOk";
+	}
+	
+	@RequestMapping(value = "/student/{studentId}")
+	public String studentId(@PathVariable String studentId, Model model) {
+		
+		model.addAttribute("studentNum", studentId);
+		
+		return "studentIdView";
+	}
+}
